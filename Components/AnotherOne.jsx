48866cfac47cd12757796360d114f4911c9563b9
GiannaDavila <giannagiannadavila@gmail.com>
2022-11-08 18:02:20 -0500

    updated

--- Components/AnotherOne.jsx
+++ Components/AnotherOne.jsx
@@ -6,12 +6,12 @@ export default function Another({getNextJoke}){
     return(
         <>
         <Button 
-        ViewComponent={LinearGradient}
-        LinearGradientProps={{
-            colors: ["#FF9800", "#F44336"],
-            start: { x: 0, y: 0.5},
-            end:  { x: 1, y: 0.5 },
-        }}
+        // ViewComponent={LinearGradient}
+        // LinearGradientProps={{
+        //     colors: ["#FF9800", "#F44336"],
+        //     start: { x: 0, y: 0.5},
+        //     end:  { x: 1, y: 0.5 },
+        // }}
         style={styles.buttonText} 
         onPress={getNextJoke} 
         title='Another One😌'></Button>
